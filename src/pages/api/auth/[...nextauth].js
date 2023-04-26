4da76b569c2a4a563b869f18f1f3eb474c76c1dc
Ujjawal Gupta <113056503+ugpec79@users.noreply.github.com>
2023-04-26 21:41:33 +0530

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: "ee2db5a9c8b7e213538e",
-      clientSecret: "7bb2e7b37925b4c44097c51441efca7453cebb52",
+      clientId: "",
+      clientSecret: "",
     }),
     // ...add more providers here
   ],
